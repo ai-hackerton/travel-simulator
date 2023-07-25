3f76d8a4b64b3c63616021c23027f102e753b59a
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-07-25 22:06:09 +0900

    Initial commit

---
+++ src/app/components/SplashScreen.jsx
@@ -0,0 +1,7 @@
+export default function SplashScreen() {
+  return (
+    <div>
+      <h2>스플래시 스크린이다.</h2>
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>홈</h2>
+    </div>
+  );
+}
