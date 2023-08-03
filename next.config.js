ea6b42354cc038f7b4b059502a955b7852100a4d
aubri61 <mingbee611@yonsei.ac.kr>
2023-08-03 22:40:43 +0900

    [feat] installed openai, removed next config env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    openAiKey: "asdfasdf1234143",
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/api/openaiApi.js
+++ src/api/openaiApi.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
+import { Configuration, OpenAIApi } from 'openai';
 
+// const BASE_URL = "";
 
-const BASE_URL = "";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 export const fetchGptAnswer = (propmt) => {
-  
+
 }
