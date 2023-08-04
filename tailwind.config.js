0dc04f50cd360bd5f940b0a99e16304720d16837
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-04 14:21:18 +0900

    [feat] 색상 회색 추가 및 로딩 스피너 컴포넌트 UI 수정
    [feat] 색상 회색 추가 및 로딩 스피너 컴포넌트 UI 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,11 @@ module.exports = {
         screen: "100dvh",
       },
       height: {
-        'screen': '100dvh',
+        screen: "100dvh",
       },
       boxShadow: {
-        'DEFAULT': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-      }
+        DEFAULT: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+      },
     },
     colors: {
       white: "#ffffff",
@@ -24,6 +24,16 @@ module.exports = {
         100: "#C63C3C",
         200: "#DA5D5D",
         300: "#C63C3C",
+      },
+      gray: {
+        100: "#F5F6F8",
+        200: "#E9EBF0",
+        300: "#DADCE2",
+        400: "#C5CAD3",
+        500: "#A6ADBC",
+        600: "#8F95A2",
+        700: "#5A6070",
+        800: "#1B1F26",
       },
     },
   },
