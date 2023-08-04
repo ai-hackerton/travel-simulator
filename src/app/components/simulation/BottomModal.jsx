23ca28009a5376fa4e6c3f817e38aeb7b69b864b
she1018 <she020108@gmail.com>
2023-08-05 00:09:16 +0900

    [feat] 하단 대화창, 캐러셀에 기능 추가

--- src/app/components/simulation/BottomModal.jsx
+++ src/app/components/simulation/BottomModal.jsx
@@ -33,6 +33,14 @@ export default function BottomModal({ day, text }) {
         }
     }, [currentIndex, text]);
 
+    // 타이핑 효과 스킵
+    const skipAnimation = () => {
+        if (currentText != text) {
+            setCurrentIndex(text.length);
+            setCurrentText(text);
+        }
+    };
+
 
     return (
         <div className="w-full h-auto flex flex-col items-center absolute bottom-5 left-1/2 transform -translate-x-1/2">
@@ -54,7 +62,9 @@ export default function BottomModal({ day, text }) {
                     <Image src={mapIcon} width={17} height={17} alt="지도 아이콘" />
                 </div>
             </div>
-            <div className="w-11/12 h-[126px] bg-black/70 rounded-[10px] shadow p-5 relative">
+            <div
+                onClick={skipAnimation}
+                className="w-11/12 h-[126px] bg-black/70 rounded-[10px] shadow p-5 relative">
                 <Image src={robotImage} width={150} height={150} alt="로봇 이미지" className="absolute -top-28 -left-5" />
                 <p id="chat" className="text-center text-base text-white font-medium">
                     {currentText}
@@ -62,7 +72,7 @@ export default function BottomModal({ day, text }) {
                 {nextAbled &&
                     <div
                         onClick={goNextPage}
-                        className="w-6 h-6 flex justify-center items-center absolute bottom-4 right-4 animate-pulse">
+                        className="w-6 h-6 flex justify-center items-center absolute bottom-3 right-4 animate-pulse">
                         <Image src={nextIcon} width={15} height={9} alt="다음 아이콘" />
                     </div>}
             </div>
