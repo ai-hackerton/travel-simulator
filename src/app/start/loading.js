efddeeeb02839b764e1b15a04073908b3818edff
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-04 14:11:38 +0900

    [feat] 랜딩 페이지 임시 UI 구현
    [feat] 랜딩 페이지 임시 UI 구현

--- src/app/start/loading.js
+++ src/app/start/loading.js
@@ -1,13 +1,13 @@
 export default function Loading() {
   return (
-    <div className="flex-col grow overflow-y-scroll px-4 justify-center items-center">
+    <div className="flex flex-col grow h-screen px-4 justify-center items-center">
       <div
         role="status"
-        className="flex flex-col justify-center items-center h-full"
+        className="flex flex-col grow justify-center items-center"
       >
         <svg
           aria-hidden="true"
-          className="inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-main-300"
+          className="inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-main-200"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      height: {
+        screen: "100dvh",
       },
     },
     colors: {
-      'white': '#ffffff',
-      'black': '#000000',
-      'main': {
-        100: '#C63C3C',
-        200: '#DA5D5D',
-        300: '#C63C3C'
-      }
+      white: "#ffffff",
+      black: "#000000",
+      main: {
+        100: "#C63C3C",
+        200: "#DA5D5D",
+        300: "#C63C3C",
+      },
     },
   },
   plugins: [],
-}
+};
