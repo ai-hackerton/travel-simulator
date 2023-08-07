d0ca89455a07f024ef76eb95452c59d0732dad6d
she1018 <she020108@gmail.com>
2023-08-07 17:32:03 +0900

    [feat] simulation page 시뮬레이션 기록, 반복 기능, 이미지 등 추가

---
+++ src/app/components/simulation/OverviewOptionModal.jsx
@@ -0,0 +1,18 @@
+"use client"
+
+import useCurrentStatus from "@/app/store/currentStatus"
+import OptionButton from "./OptionButton"
+import useSimulationIndex from "@/app/store/simulationIndex";
+
+export default function OverviewOptionModal() {
+    const { increaseIndex: goNextPage, decreaseIndex: goPrevPage } = useSimulationIndex();
+
+    return (
+        <div className="w-full h-[242px] absolute top-[40%] transform -translate-y-1/2">
+            <div className="w-full h-full flex flex-col justify-center items-center space-y-[14px] relative">
+                <OptionButton text="여기여기여기여기로 출발!" onClick={goNextPage} />
+                <OptionButton text="다른 곳도 좀 둘러볼래" onClick={goPrevPage} />
+            </div>
+        </div>
+    )
+}
