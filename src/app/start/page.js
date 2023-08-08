f1d5a1964a9c87301dec778a20894c7c2c761648
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-08 11:12:25 +0900

    [fix] Input Modal UI 수정
    [fix] Input Modal UI 수정

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -48,16 +48,18 @@ export default function StartingPage() {
     case 1:
       content = (
         <>
-          <p className="font-medium text-white text-lg">
-            당신의 이름을 알려주세요!
-          </p>
-          <div className="w-full justify-center items-center">
-            <input
-              className="w-4/5 max-w-fit bg-white bg-opacity-80 px-4 py-2 rounded-xl text-black"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+          <div className="flex flex-col items-center">
+            <p className="font-medium text-white text-lg">
+              당신의 이름을 알려주세요!
+            </p>
+            <div className="w-full max-w-fit bg-white bg-opacity-80 px-4 py-2 rounded-xl text-black">
+              <input
+                className="w-full"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
           </div>
         </>
       );
