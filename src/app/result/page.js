51f2bc1e093d244371e28907b4f49b60050a3683
aubri61 <mingbee611@yonsei.ac.kr>
2023-08-08 13:53:39 +0900

    [feat] result/login 에서 더미 설정, result -> result/login 서로 리다이렉션

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,9 +1,27 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import useSimulationHistory from "../store/simulationHistory";
+import useTravelSettingsStore from "../store/travelSettings";
 
 export default function Page() {
   const { simulationHistory } = useSimulationHistory();
+  const { travelSettings } = useTravelSettingsStore();
+  const router = useRouter();
   console.log("result에용 ", simulationHistory);
+
+  // TODO: 시뮬레이션 히스토리 있는지 없는지.. 체크 (로그인 or not)
+
+  useEffect(() => {
+    if (!simulationHistory.length || !travelSettings) {
+      router.push("/result/login");
+    }
+  }, []);
+
+  // if (simulationHistory.length) {
   return <div>hihi</div>;
+  // } else {
+  //   return <div>아무 것도 없네용~ 로그인 페이지로 이동이동~</div>;
+  // }
 }
