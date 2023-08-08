64c9edfc954f4755aa8568453fb3c02a3cf086b9
aubri61 <mingbee611@yonsei.ac.kr>
2023-08-08 23:19:34 +0900

    [feat] reuslt 페이지에 뒤로가기 버튼 추가

--- src/app/result/login/page.js
+++ src/app/result/login/page.js
@@ -1,13 +1,9 @@
-
 import Image from "next/image";
-// import resultImage from "public/images/resultImage.jpg";
 import resultImage2 from "public/images/resultImage2.jpg";
 
 import ResultLoginBox from "@/app/components/result/ResultLoginBox";
 
 export default function Page() {
-
-
   return (
     <div className="h-screen ">
       <Image
@@ -18,13 +14,6 @@ export default function Page() {
         alt="배경화면"
       />
       <div className="absolute py-5 px-5 bg-black/40 backdrop-blur-[1px] inset-0  flex flex-col ">
-        {/* <button
-          className="bg-main-200 text-white font-xl font-bold self-center rounded-2xl shadow px-10 py-2"
-          onClick={onMoveClick}
-        >
-          로그인
-        </button> */}
-
         <ResultLoginBox />
       </div>
     </div>
