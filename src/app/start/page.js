88a3f67a8ded73903fd8f9070df5571417853fd7
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-08 11:08:59 +0900

    [fix] input 모달 ui 수정
    [fix] input 모달 ui 수정

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -51,9 +51,9 @@ export default function StartingPage() {
           <p className="font-medium text-white text-lg">
             당신의 이름을 알려주세요!
           </p>
-          <div className="w-full">
+          <div className="w-full justify-center items-center">
             <input
-              className="w-full bg-white bg-opacity-80 px-4 py-2 rounded-xl text-black"
+              className="w-4/5 max-w-fit bg-white bg-opacity-80 px-4 py-2 rounded-xl text-black"
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
