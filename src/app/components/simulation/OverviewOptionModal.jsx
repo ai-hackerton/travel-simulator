08a7acbfab185c853e88dc241dca50e1c779973b
she1018 <she020108@gmail.com>
2023-08-09 03:49:08 +0900

    [fix] 시뮬레이션 멘트, 개요페이지 로직 수정

--- src/app/components/simulation/OverviewOptionModal.jsx
+++ src/app/components/simulation/OverviewOptionModal.jsx
@@ -10,7 +10,7 @@ export default function OverviewOptionModal() {
     return (
         <div className="w-full h-[242px] absolute top-[40%] transform -translate-y-1/2">
             <div className="w-full h-full flex flex-col justify-center items-center space-y-[14px] relative">
-                <OptionButton text="여기여기여기여기로 출발!" onClick={goNextPage} />
+                <OptionButton text="이곳으로 출발!" onClick={goNextPage} />
                 <OptionButton text="다른 곳도 좀 둘러볼래" onClick={goPrevPage} />
             </div>
         </div>
