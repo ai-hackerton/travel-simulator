86f56739c3e5fb663945d18a0ebbf20498933ac6
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-08 10:46:51 +0900

    [style] 스플래시 페이지 컴포넌트 UI 수정
    [style] 스플래시 페이지 컴포넌트 UI 수정

--- src/app/page.js
+++ src/app/page.js
@@ -31,11 +31,11 @@ export default function Home() {
             href="/start"
             className="w-full flex flex-col justify-center items-center"
           >
-            <button className="w-4/5 bg-white px-4 py-2 text-center text-black text-lg font-bold rounded-xl shadow-md hover:bg-red-300">
+            <button className="w-4/5 bg-white bg-opacity-90 px-4 py-2 text-center text-black text-lg font-bold rounded-xl shadow-md hover:bg-red-300">
               새로운 시뮬레이션 시작하기
             </button>
           </Link>
-          <button className="w-4/5 bg-white px-4 py-2 text-center text-black text-lg font-bold rounded-xl shadow-md hover:bg-red-300">
+          <button className="w-4/5 bg-white bg-opacity-90 px-4 py-2 text-center text-black text-lg font-bold rounded-xl shadow-md hover:bg-red-300">
             시뮬레이션 기록보기
           </button>
         </div>
